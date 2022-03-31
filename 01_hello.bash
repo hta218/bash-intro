1a4de18f2a6512785e4d8aae4e1f42c4324fa3ce
Leo Huynh <huynhtuananh21895@gmail.com>
2022-04-01 00:53:54 +0700

    Add shebang note

--- 01_hello.bash
+++ 01_hello.bash
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# The first line is called [shebang](http://en.wikipedia.org/wiki/Shebang_%28Unix%29)
+# That line in any script determines the script's ability to be executed like a standalone executable without typing sh, bash, python, php etc beforehand in the terminal.
+
 # Use echo to print a string
 # Use $ to print a variable
 echo "Hello, world! It's $(whoami) on $(hostname)!"
