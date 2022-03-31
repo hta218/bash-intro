d7cf110cd78e749a75da893a2f38ab0efd0cbe30
Leo Huynh <huynhtuananh21895@gmail.com>
2022-04-01 00:50:46 +0700

    Positional parameters and default values

--- 03_parameters.bash
+++ 03_parameters.bash
@@ -2,11 +2,11 @@
 
 ### Positional parameters
 # Positional parameters are the parameters that are passed to a script.
-# $0 --> The name of the script.
-# $1 .. $9 --> The 1st through 9th positional parameters.
-# $# --> The number of positional parameters.
-# $* or $@ --> All positional parameters.
-# $FUNCNAME --> The name of the function.
+# $0          -->   The name of the script.
+# $1 .. $9    -->   The 1st through 9th positional parameters.
+# $#          -->   The number of positional parameters.
+# $* or $@    -->   All positional parameters.
+# $FUNCNAME   -->   The name of the function.
 echo "Positional parameters: (Try typing \`bash ./03_parameters.bash foo bar\`)"
 echo "The script name is \"$0\""
 echo "The script has $# positional parameters"
@@ -20,6 +20,6 @@ THIRD_PARAM=${THIRD_PARAM:-'This is the default value of the 3rd param if it is 
 echo "The 3rd positional parameter is \"$THIRD_PARAM\""
 
 ### Challenge
-# echo "1: $1"
-# echo "3: $3"
-# echo "5: $5"
+echo "1: $1"
+echo "3: $3"
+echo "5: $5"
