6bd40dfb91dd82cdb05542c1da1a9aa15a475fbc
Leo Huynh <huynhtuananh21895@gmail.com>
2022-04-01 10:21:19 +0700

    Brace/Arithmetic expansion, command substitution

--- 05_expansions.bash
+++ 05_expansions.bash
@@ -24,3 +24,12 @@ echo "This moment: $this_moment"
 # Syntax: $(( expression ))
 echo "10 + 5 = $(( 10+5 ))"
 echo "6 + (10 * 2) / 5 = $(( 6 + (10 * 2) / 5 ))"
+
+### Challenge
+result=$(( $1 * ($2 + $3) ))
+echo project-$result/{src,dest,test}/{index,util}.js
+
+### Extra
+# Try `mkdir -p project/{src,test}/`
+# Then `touch project/{src,test}/{index,util}.js`
+# These commands will create a project folder with a src and test folder inside, and two files inside each folder.
