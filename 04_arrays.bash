91c6793ae3ae8db49e9a014f5051d5b5d30ebe08
Leo Huynh <huynhtuananh21895@gmail.com>
2022-04-01 09:21:27 +0700

    Adding & deleting element from array

--- 04_arrays.bash
+++ 04_arrays.bash
@@ -5,11 +5,19 @@
 planets[0]="Mercury"
 planets[1]="Venus"
 planets[2]="Earth"
-echo ${planets[*]}
+echo "The 3 first planets in the solar system are: ${planets[*]}"
 
 # Or array can be created by using the array() function:
-planets=(Mercury Venus Earth Mars Jupiter Saturn Uranus Neptune Pluto)
-echo ${planets[@]}
+planets=(Mercury Venus Earth Mars Jupiter Saturn)
+echo "The 6 first planets in the solar system are: ${planets[@]}"
 
 # Slice an array with array[*]:start:length
-echo "3 planets start from the 2nd one are: ${planets[*]:1:3}"
+echo "The 3 planets start from the 2nd one are: ${planets[*]:1:3}"
+
+# Add elements to an array:
+planets=(${planets[*]} Uranus Neptune Pluto)
+echo "All planets in the solar system are: ${planets[*]}"
+
+# Delete an element from an array with `unset`:
+unset planets[0]
+echo "All planets in the solar system except the first one are: ${planets[*]}"
