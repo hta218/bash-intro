009de64625e8e4de93961188d0e6a7fb24bc72c1
Leo Huynh <huynhtuananh21895@gmail.com>
2022-04-02 14:19:53 +0700

    Function

--- 10_function.bash
+++ 10_function.bash
@@ -1,29 +1,29 @@
 #!/usr/bin/env bash
 
 # # Function must be defined before we can use it
-# my_func () {
-#   echo "Hello, it's $USER, this is my 1st bash function! This function name is $FUNCNAME"
-# }
+my_func () {
+  echo "Hello, it's $USER, this is my 1st bash function! This function name is $FUNCNAME"
+}
 
-# my_func
+my_func
 
-# # Function parameters
-# greeting () {
-#   local foo="Foooo" # Local variables are only available in the function
-#   bar="Bazzz"
-#   if [[ $1 ]]; then
-#     echo "Hello, $1"
-#   else
-#     echo "Hello, the unknown!"
-#   fi
-#   return 0
-# }
+# Function parameters
+greeting () {
+  local foo="Foooo" # Local variables are only available in the function
+  bar="Bazzz"
+  if [[ $1 ]]; then
+    echo "Hello, $1"
+  else
+    echo "Hello, the unknown!"
+  fi
+  return 0
+}
 
-# greeting "John"
-# greeting
+greeting "John"
+greeting
 
-# echo "foo is: $foo"
-# echo "baz is: $bar"
+echo "foo is: $foo"
+echo "baz is: $bar"
 
 ### Challenge
 print() {
