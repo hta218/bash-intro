897ddde4439b42e256bdb413939ca0f2f355a052
Leo Huynh <huynhtuananh21895@gmail.com>
2022-04-02 10:28:19 +0700

    Resolve case challenge

--- 08_case.bash
+++ 08_case.bash
@@ -1,12 +1,24 @@
 #!/usr/bin/env bash
 
+# case "$1" in
+#   (apple)
+#     echo "Apple !!" ;;
+#   (banana)
+#     echo "Bananaa !!" ;;
+#   (orange|lemon)
+#     echo "Eww !!" ;;
+#   (*)
+#     echo "Unknown fruit!" ;;
+# esac
+
+### Challenge
 case "$1" in
-  (apple)
-    echo "Apple !!" ;;
-  (banana)
-    echo "Bananaa !!" ;;
-  (orange|lemon)
-    echo "Eww !!" ;;
+  (jpg|jpeg)
+    echo "It is jpeg." ;;
+  (png)
+    echo "It is png." ;;
+  (gif)
+    echo "It is gif." ;;
   (*)
-    echo "Unknown fruit!" ;;
+    echo "$1 is not an image!" ;;
 esac
